afa3830c5c295573ac69ff2510e3095182e2ed84
Tomas Linhart <tomas@linhart.me>
2013-01-27 21:44:51 +0100

    Fix output format for methods

--- Scrappy/Compiler/Model/MethodModel.cs
+++ Scrappy/Compiler/Model/MethodModel.cs
@@ -36,7 +36,7 @@ namespace Scrappy.Compiler.Model
 		{
 			StringBuilder builder = new StringBuilder();
 
-            builder.AppendFormat("\t\t\t<method name=\"{0}::{1}\" type=\"{2}\">", Name, Arguments.Count, Type);
+            builder.AppendFormat("\t\t\t<method name=\"{0}\" type=\"{1}\">", Name, Type);
 
 			builder.AppendLine();
 
